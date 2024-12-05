7e06cf4e5ed8c09a31aef6191b59b580335a2ad6
rafaguzmang <rafaguzmang@users.noreply.github.com>
2024-12-05 15:24:46 -0600

    Configuración

--- src/polyfills.ts
+++
@@ -1,5 +0,0 @@
-import 'core-js/es/array';
-import 'core-js/es/object';
-import 'core-js/es/promise';
-import 'core-js/features/symbol'; // Polyfill para Symbol
-import 'zone.js'; // Este polyfill es obligatorio para Angular
