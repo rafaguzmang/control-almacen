6f4098cb33c59abc45090edf203b0fd336d81be7
Rafael Guzmán Granados <55261233+rafaguzmang@users.noreply.github.com>
2025-04-10 09:11:57 -0600

    Se agrega opción de materiales así como la mejora del buscador en ordenes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,20 @@
-import { provideRouter, Routes } from '@angular/router';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
 import { InventarioComponent } from './inventario/inventario.component';
 import { ConsumiblesComponent } from './consumibles/consumibles.component';
 import { SolicitudmaterialComponent } from './solicitudmaterial/solicitudmaterial.component';
 import { EntransitoComponent } from './entransito/entransito.component';
 import { HerramientasComponent } from './herramientas/herramientas.component';
 import { HomeComponent } from './home/home.component';
+import { MaterialesComponent } from './materiales/materiales.component';
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'consumibles', component: ConsumiblesComponent },
     { path: 'ordenes', component: SolicitudmaterialComponent },
     { path: 'transito', component: EntransitoComponent },
     { path: 'inventario', component: InventarioComponent },
     { path: 'herramientas', component: HerramientasComponent },
+    { path: '', component: MaterialesComponent },
     
 ];
